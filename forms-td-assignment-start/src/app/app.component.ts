47dcb32b4d625bf666f2fcc1cc6b5a13ccf40143
nkolchakov <nikola.kolchakov@gmail.com>
2019-02-20 20:34:44 +0200

    display input data in template

--- forms-td-assignment-start/src/app/app.component.ts
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -1,20 +1,29 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
-import {NgModel} from '@angular/forms';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements OnInit {
 
   subscriptions = ['Basic', 'Advanced', 'Pro'];
   defaultSub: string;
+  user: any = {};
+  submitted = false;
 
-  @ViewChild('f') subForm: NgModel;
+  @ViewChild('f') subForm: NgForm;
 
   onSubmit() {
-    console.log(this.subForm);
+    console.log(this.subForm.form.value);
+    this.user.email = this.subForm.form.value.email;
+    this.user.password = this.subForm.form.value.password;
+    this.user.sub = this.subForm.form.value.subscription;
+
+    this.submitted = true;
+    this.subForm.reset();
   }
 
   ngOnInit(): void {
