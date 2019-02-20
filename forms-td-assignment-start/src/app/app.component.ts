ec32f1af2631043fa8be5543ba7611e9ef8377c0
nkolchakov <nikola.kolchakov@gmail.com>
2019-02-20 19:32:22 +0200

    layout the form

--- forms-td-assignment-start/src/app/app.component.ts
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {NgModel} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
+  subscriptions = ['Basic', 'Advanced', 'Pro'];
+  defaultSub: string;
+
+  @ViewChild('f') subForm: NgModel;
+
+  onSubmit() {
+    console.log(this.subForm);
+  }
+
+  ngOnInit(): void {
+    this.defaultSub = this.subscriptions[1];
+  }
 }
